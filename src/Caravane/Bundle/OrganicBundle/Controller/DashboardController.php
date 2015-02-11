c0f5c391059ee744d408446f16702665741d4791
Eric Grivilers <eric@caravanemedia.com>
2015-02-11 09:57:49 +0100

    dashboard

--- src/Caravane/Bundle/OrganicBundle/Controller/DashboardController.php
+++ src/Caravane/Bundle/OrganicBundle/Controller/DashboardController.php
@@ -2,23 +2,34 @@
 
 namespace Caravane\Bundle\OrganicBundle\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class DashboardController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
 
     	if(!$this->get('security.context')->isGranted('ROLE_USER', $this->getUser())) {
-
     		return $this->redirect($this->generateUrl('fos_user_security_login'));
     	}
-            $em = $this->getDoctrine()->getManager();
+        
 
+        $em = $this->getDoctrine()->getManager();
 
+        if($request->query->get('all')) {
+            $messages=$em->getRepository('CaravaneOrganicBundle:Chat')->findBy(array('messagetype'=>'logs'),array('messagedate'=>' desc'));
+            $offres=$em->getRepository('CaravaneOrganicBundle:Offre')->findBy(array(),array('startbuild'=>' desc'));
+            $jobs=$em->getRepository('CaravaneOrganicBundle:Job')->findBy(array(),array('startbuild'=>' desc'));
+        }
+        else {
             $messages=$em->getRepository('CaravaneOrganicBundle:Chat')->findBy(array('messagetype'=>'logs','target'=>5),array('messagedate'=>' desc'));
-        $offres=$em->getRepository('CaravaneOrganicBundle:Offre')->findBy(array('userid'=>$this->getUser()->getId()),array('startbuild'=>' desc'));
-        $jobs=$em->getRepository('CaravaneOrganicBundle:Job')->findBy(array('userid'=>$this->getUser()->getId()),array('startbuild'=>' desc'));
+            $offres=$em->getRepository('CaravaneOrganicBundle:Offre')->findBy(array('userid'=>$this->getUser()->getId()),array('startbuild'=>' desc'));
+            $jobs=$em->getRepository('CaravaneOrganicBundle:Job')->findBy(array('userid'=>$this->getUser()->getId()),array('startbuild'=>' desc'));
+        }
+
 
 
     	return $this->render('CaravaneOrganicBundle:Dashboard:index.html.twig', array(
